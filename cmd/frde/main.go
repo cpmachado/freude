ed58b98db018feb01cdf590bd5b3736b3bb91ec0
cpmachado <cpmachado@protonmail.com>
2025-02-22 11:34:50 +0000

    Add version to frde, and gitignore

--- cmd/frde/main.go
+++ cmd/frde/main.go
@@ -5,11 +5,26 @@ configuration file.
 package main
 
 import (
+	"flag"
 	"fmt"
+	"os"
 
 	"go.cpmachado.pt/freude"
 )
 
+var Version = "0.0.0"
+
+func init() {
+	var v bool
+	flag.BoolVar(&v, "version", false, "Display version and exit")
+	flag.Parse()
+
+	if v {
+		fmt.Printf("frde version frde%s", Version)
+		os.Exit(0)
+	}
+}
+
 func main() {
-	fmt.Printf("Hello freude at version %s", freude.Version)
+	fmt.Printf("Hello freude at version %s\n", freude.Version)
 }
